6eeb33b0ff0fb836cd32bfd5fe239671dbb24b2d
D-Aldin <dobricaldin@gmail.com>
2025-07-08 23:34:55 +0200

    download the font Nunito from Google fonts and implement it in the styles.scss and add intro-component to the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { IntroComponent } from './intro/intro.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, IntroComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'dabubble';
