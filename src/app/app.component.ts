c1e52eb2ff81e1fc159c1913263ebea7c0b76535
Muzammal Anwar <muzammalanwar556@gmail.com>
2025-07-09 18:26:44 +0200

    routes defined, components generated and favicon set

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,16 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { IntroComponent } from './intro/intro.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LegalNoticeComponent } from './pages/legal-notice/legal-notice.component';
+import { PrivacyPolicyComponent } from './pages/privacy-policy/privacy-policy.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, IntroComponent],
+  imports: [CommonModule, RouterOutlet, IntroComponent, DashboardComponent, LegalNoticeComponent, PrivacyPolicyComponent, RegisterComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LegalNoticeComponent } from './pages/legal-notice/legal-notice.component';
+import { PrivacyPolicyComponent } from './pages/privacy-policy/privacy-policy.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: LoginComponent },
+    { path: '/dashboard', component: DashboardComponent },
+    { path: '/register', component: RegisterComponent },
+    { path: '/legal-notice', component: LegalNoticeComponent }, 
+    { path: '/privacy-policy', component: PrivacyPolicyComponent },
+    { path: '**', component: LoginComponent }
+];
