30c285f2676ee88ff3181f598018abb9e3e195da
D-Aldin <dobricaldin@gmail.com>
2025-07-10 23:35:32 +0200

    create timestamp-line component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ProfileCardComponent } from './shared/profile-card/profile-card.component';
 
 @Component({
   selector: 'app-root',
---
+++ src/app/shared/timestamp-line/timestamp-line.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-timestamp-line',
+  standalone: true,
+  imports: [],
+  templateUrl: './timestamp-line.component.html',
+  styleUrl: './timestamp-line.component.scss'
+})
+export class TimestampLineComponent {
+
+}
