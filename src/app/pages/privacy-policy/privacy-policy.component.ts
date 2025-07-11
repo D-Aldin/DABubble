08cd5c8511752550c02f01b9982e52ad07465934
mian shargeel <call2poma@gmail.com>
2025-07-11 17:26:13 +0200

    Updated Privacy-Policy Design

--- src/app/pages/privacy-policy/privacy-policy.component.ts
+++ src/app/pages/privacy-policy/privacy-policy.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/header/header.component';
+import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-privacy-policy',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, RouterModule],
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.scss'
 })
