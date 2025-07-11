840d92fa36f43c18303882297bee02a6fcc8c6d1
D-Aldin <dobricaldin@gmail.com>
2025-07-11 20:46:21 +0200

    create a thread component and create a mixin for the close button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ThreadComponent } from './shared/thread/thread.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ThreadComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
