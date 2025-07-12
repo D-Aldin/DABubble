b3562b5c93d38053ae8a2d4343d965cf368e35ec
D-Aldin <dobricaldin@gmail.com>
2025-07-12 15:09:26 +0200

    create chat-box

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ThreadComponent } from './shared/thread/thread.component';
+import { ChatBoxComponent } from './shared/chat-box/chat-box.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ThreadComponent],
+  imports: [CommonModule, RouterOutlet, ThreadComponent, ChatBoxComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
---
+++ src/app/shared/chat-box/chat-box.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-box',
+  standalone: true,
+  imports: [],
+  templateUrl: './chat-box.component.html',
+  styleUrl: './chat-box.component.scss',
+})
+export class ChatBoxComponent {
+  @Input({ required: true }) src!: string;
+  @Input({ required: true }) userName!: string;
+  @Input({ required: true }) time!: string;
+  @Input({ required: true }) message!: string;
+
+  userMe!: true;
+}
