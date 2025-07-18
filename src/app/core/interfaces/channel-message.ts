16b217c43cac45646fb32157c75e552b4a5e8bf6
mian shargeel <call2poma@gmail.com>
2025-07-18 14:32:30 +0200

    added channel messaging service functions and ChannelMessagesComponent to display real-time messages

---
+++ src/app/core/interfaces/channel-message.ts
@@ -0,0 +1,10 @@
+import { Timestamp } from '@angular/fire/firestore';
+
+export interface ChannelMessage {
+  id?: string;
+  senderId: string;
+  text: string;
+  timestamp: Timestamp;
+  reactions?: { [userId: string]: string };
+  threadId?: string;
+}
--- src/app/core/services/channel.service.ts
+++ src/app/core/services/channel.service.ts
@@ -1,7 +1,9 @@
 import { inject, Injectable } from '@angular/core';
-import { Firestore, collection, collectionData, addDoc, updateDoc, arrayUnion, doc, docData } from '@angular/fire/firestore';
+import { Firestore, collection, collectionData, addDoc, updateDoc, arrayUnion, doc, docData, serverTimestamp,  query,
+  orderBy } from '@angular/fire/firestore';
 import { Observable, Subject, } from 'rxjs';
 import { Channel } from '../interfaces/channel';
+import { ChannelMessage } from '../interfaces/channel-message';
 
 @Injectable({
   providedIn: 'root'
@@ -40,6 +42,35 @@ export class ChannelService {
   updateChannel(channelId: string, data: Partial<Channel>) {
     const channelDoc = doc(this.firestore, 'channels', channelId);
     return updateDoc(channelDoc, data);
+  }
+
+  sendChannelMessage(channelId: string, senderId: string, text: string) {
+    const messagesRef = collection(this.firestore, `channels/${channelId}/messages`);
+    return addDoc(messagesRef, {
+      senderId,
+      text,
+      timestamp: serverTimestamp(),
+    });
+  }
+
+  listenToChannelMessages(channelId: string): Observable<ChannelMessage[]> {
+    const messagesRef = collection(this.firestore, `channels/${channelId}/messages`);
+    const q = query(messagesRef, orderBy('timestamp'));
+    return collectionData(q, { idField: 'id' }) as Observable<ChannelMessage[]>;
+  }
+
+  addReaction(channelId: string, messageId: string, userId: string, emoji: string) {
+    const msgRef = doc(this.firestore, `channels/${channelId}/messages/${messageId}`);
+    return updateDoc(msgRef, {
+      [`reactions.${userId}`]: emoji
+    });
+  }
+
+  addThreadIdToMessage(channelId: string, messageId: string, threadId: string) {
+    const msgRef = doc(this.firestore, `channels/${channelId}/messages/${messageId}`);
+    return updateDoc(msgRef, {
+      threadId
+    });
   }
 
 }
