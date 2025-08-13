3cc1f684bd22bba2ac2413088c2161e148e01433
mian shargeel <call2poma@gmail.com>
2025-08-13 13:05:39 +0200

    Added(channel-reactions): allow up to 20 emoji reactions per user; Channel UI: show first 4 reaction chips; collapse remainder into '+X'

--- src/app/core/interfaces/channel-message.ts
+++ src/app/core/interfaces/channel-message.ts
@@ -1,11 +1,12 @@
 import { Timestamp } from '@angular/fire/firestore';
+import { LegacyReactions, NewReactions } from '../../core/interfaces/message';
 
 export interface ChannelMessage {
   id?: string;
   senderId: string;
   text: string;
   timestamp: Timestamp;
-  reactions?: { [userId: string]: string };
+  reactions?: LegacyReactions | NewReactions;
   threadId?: string;
   replyCount?: number;
   lastReplyTimestamp?: Timestamp | Date;
